4956706b1fb7bc4a6817c43cbf9e78695f909885
emdadripon <eriponctg@gmail.com>
2026-01-30 15:39:41 -0500

    change

--- envs/prod/backend.tf
+++ envs/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket" 
     key            = "prod/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-locks"
