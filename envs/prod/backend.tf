96ad79c64bfea78aa04ee26b41d4b947dd3dc18c
emdadripon <eriponctg@gmail.com>
2026-01-30 15:32:32 -0500

    yaml added

---
+++ envs/prod/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "prod/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
